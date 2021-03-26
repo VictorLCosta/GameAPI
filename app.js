98b89c1614b23593f160f3f2dbb1bac0e5accd25
VictorLCosta <victorlc2019@outlook.com>
2021-03-26 14:51:54 -0300

    API find one game and status code

--- app.js
+++ app.js
@@ -5,10 +5,59 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-//Rotas
-app.get('/', (req, res) => 
+var DB = 
 {
-    
+    games: 
+    [
+        {
+            id: 1,
+            title: "Half Life 2",
+            year: 2012,
+            price: 20
+        },
+        {
+            id: 2,
+            title: "Metal Gear Solid 3 - Snake Eater",
+            year: 2005,
+            price: 20
+        },
+        {
+            id: 3,
+            title: "Elder Scrolls 5 - Skyrim",
+            year: 2011,
+            price: 40
+        }
+    ]
+}
+
+//Rotas
+app.get('/games', (req, res) => 
+{
+    res.statusCode = 200;
+    res.json();
+});
+
+app.get('/games/:id', (req, res) => 
+{
+    if(!isNaN(req.params.id))
+    {
+        var id = parseInt(req.params.id);
+        var game = DB.games.find(g => g.id == id);
+
+        if(game != undefined)
+        {
+            res.statusCode = 200;
+            res.json(game);
+        }
+        else
+        {
+            res.sendStatus(404);
+        }
+    }
+    else
+    {
+        res.sendStatus(400);
+    }
 });
 
 app.listen(8080, () => 
